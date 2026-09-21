c2fdd7d6eef9525e60196ccf7d4e7738ff77a131
Anton Amirian <extuessl@gmail.com>
2026-09-21 22:43:55 +0300

    replace dictionary with concurrent dictionary

--- DESIGN.md
+++ DESIGN.md
@@ -2,6 +2,7 @@
 
 Kvas is an in-memory key-value store consisting of a main writer service and 2 replicas
 that can read the key-value pair but not add new values to the store.
+Default port is `5757`
 
 It consists of three commands:
 
@@ -11,6 +12,13 @@ It consists of three commands:
 
 The server will return an empty value on a missing key, and willoverwrite a key if it already exists.
 
+# Replication details
+
+Primary server listens on port `57570` for connections from replicas to send them data
+to replicate.
+
+When replica starts it connects to said port and listens for SET/DEL events from primary.
+
 # Guarantess
 
 A client can reply for the data to be stored in the main server. Though the data will
@@ -18,7 +26,7 @@ eventually propogate to the replicas, it is not immediate.
 
 # Concurrency and backpressure
 
-`SET` and `DEL` can be accessed only from a single thread.
+`SET` and `DEL` can write only from a single thread.
 
 Connection pool is limited to 128 clients and a timeout of 2 seconds to limit pressure.
 
--- src/server/CacheStore.cs
+++ src/server/CacheStore.cs
@@ -1,14 +1,13 @@
+using System.Collections.Concurrent;
 using server.Interfaces;
 
 namespace server;
 
 public sealed class CacheStore : ICacheStore
 {
-    private readonly SemaphoreSlim _semaphore = new(1, 1);
-    
-    private readonly Dictionary<string, string> _dictionary = new();
+    private readonly ConcurrentDictionary<string, string> _dictionary = new();
 
-    public async Task SetAsync(string key, string value)
+    public Task SetAsync(string key, string value)
     {
         if (string.IsNullOrEmpty(key))
         {
@@ -20,52 +19,35 @@ public sealed class CacheStore : ICacheStore
             throw new ArgumentNullException(nameof(value));
         }
 
-        await _semaphore.WaitAsync();
-        try
-        {
-            _dictionary[key] = value;
-        }
-        finally
-        {
-            _semaphore.Release();
-        }
+        _dictionary[key] = value;
+        return Task.CompletedTask;
     }
 
-    public async Task<string> GetAsync(string key)
+    public Task<string> GetAsync(string key)
     {
         if (string.IsNullOrEmpty(key))
         {
             throw new ArgumentNullException(nameof(key));
         }
 
-        if (_dictionary.TryGetValue(key, out var value))
-        {
-            return await Task.FromResult(value);
-        }
-
-        return await Task.FromResult(string.Empty);
+        return Task.FromResult(
+            _dictionary.TryGetValue(key, out var value)
+                ? value
+                : string.Empty);
     }
 
-    public async Task RemoveAsync(string key)
+    public Task RemoveAsync(string key)
     {
         if (string.IsNullOrEmpty(key))
         {
             throw new ArgumentNullException(nameof(key));
         }
 
-        if (!_dictionary.ContainsKey(key))
+        if (!_dictionary.TryRemove(key, out _))
         {
             throw new KeyNotFoundException();
         }
 
-        await _semaphore.WaitAsync();
-        try
-        {
-            _dictionary.Remove(key);
-        }
-        finally
-        {
-            _semaphore.Release();
-        }
+        return Task.CompletedTask;
     }
 }
